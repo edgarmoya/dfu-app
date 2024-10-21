7b9019dc2d309ad0633186be1182fdde1ff55458
edgarmoya <edgarcmoya@gmail.com>
2024-10-21 12:09:03 -0400

    refactor: Organized code structure and improved readability

--- app.py
+++ app.py
@@ -15,19 +15,6 @@ st.set_page_config(
 # Barra lateral
 st.sidebar.header("Configuración del modelo")
 
-# Ruta del modelo de detección
-detection_model_path = Path(settings.DETECTION_MODEL)
-
-# Cargar el modelo
-try:
-    model = load_model(detection_model_path)
-except Exception as ex:
-    st.error(f"No se pudo cargar el modelo. Verifique la ruta especificada: {detection_model_path}")
-    st.error(ex)
-
-# Título de la página principal
-st.title("Detección de UPD")
-
 # Opciones del modelo
 confidence = float(st.sidebar.slider(
     "Seleccionar confianza de detección", 0, 100, 30, help='Probabilidad de certeza en la detección de la úlcera')) / 100  # Control deslizante para la confianza del modelo
@@ -41,6 +28,19 @@ source_img = st.sidebar.file_uploader(
     help='Imagen del pie que desea analizar', 
     type=("jpg", "jpeg", "png"), 
     accept_multiple_files=False)
+
+# Título de la página principal
+st.title("Detección de UPD")
+
+# Ruta del modelo de detección
+detection_model_path = Path(settings.DETECTION_MODEL)
+
+# Cargar el modelo
+try:
+    model = load_model(detection_model_path)
+except Exception as ex:
+    st.error(f"No se pudo cargar el modelo. Verifique la ruta especificada: {detection_model_path}")
+    st.error(ex)
 
 # Verificar si la imagen original ha cambiado
 if 'uploaded_image' in st.session_state:
@@ -94,10 +94,9 @@ if source_img is not None:
                 st.info('No se han detectado ulceraciones', icon="ℹ️")
 else:
     camera_svg = '''
-            <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 24 24" width="24" height="24">
-                <circle cx="16" cy="8.011" r="2.5"/><path d="M23,16a1,1,0,0,0-1,1v2a3,3,0,0,1-3,3H17a1,1,0,0,0,0,2h2a5.006,5.006,0,0,0,5-5V17A1,1,0,0,0,23,16Z"/><path d="M1,8A1,1,0,0,0,2,7V5A3,3,0,0,1,5,2H7A1,1,0,0,0,7,0H5A5.006,5.006,0,0,0,0,5V7A1,1,0,0,0,1,8Z"/><path d="M7,22H5a3,3,0,0,1-3-3V17a1,1,0,0,0-2,0v2a5.006,5.006,0,0,0,5,5H7a1,1,0,0,0,0-2Z"/><path d="M19,0H17a1,1,0,0,0,0,2h2a3,3,0,0,1,3,3V7a1,1,0,0,0,2,0V5A5.006,5.006,0,0,0,19,0Z"/><path d="M18.707,17.293,11.121,9.707a3,3,0,0,0-4.242,0L4.586,12A2,2,0,0,0,4,13.414V16a3,3,0,0,0,3,3H18a1,1,0,0,0,.707-1.707Z"/>
-            </svg>
-        '''
+        <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 24 24" width="24" height="24">
+            <circle cx="16" cy="8.011" r="2.5"/><path d="M23,16a1,1,0,0,0-1,1v2a3,3,0,0,1-3,3H17a1,1,0,0,0,0,2h2a5.006,5.006,0,0,0,5-5V17A1,1,0,0,0,23,16Z"/><path d="M1,8A1,1,0,0,0,2,7V5A3,3,0,0,1,5,2H7A1,1,0,0,0,7,0H5A5.006,5.006,0,0,0,0,5V7A1,1,0,0,0,1,8Z"/><path d="M7,22H5a3,3,0,0,1-3-3V17a1,1,0,0,0-2,0v2a5.006,5.006,0,0,0,5,5H7a1,1,0,0,0,0-2Z"/><path d="M19,0H17a1,1,0,0,0,0,2h2a3,3,0,0,1,3,3V7a1,1,0,0,0,2,0V5A5.006,5.006,0,0,0,19,0Z"/><path d="M18.707,17.293,11.121,9.707a3,3,0,0,0-4.242,0L4.586,12A2,2,0,0,0,4,13.414V16a3,3,0,0,0,3,3H18a1,1,0,0,0,.707-1.707Z"/>
+        </svg>'''
 
     with st.container(border=True):
         st.markdown(
